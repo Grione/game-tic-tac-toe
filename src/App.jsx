91424e441183f7a075ce95b0265377a771efb1d6
Grigoriy Gavrilov <greeb.spb@gmail.com>
2024-05-29 12:41:14 +0500

    remove extra state

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function setActivePlayer(gameTurns) {
+  let activePlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    activePlayer = 'O';
+  }
+
+  return activePlayer;
+}
+
 function App() {
-  const [playerActive, setPlayerActive] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
 
+  let currentPlayer = setActivePlayer(gameTurns);
+
   function handlerOnChange(rowIndex, colIndex) {
-    setPlayerActive((prevPlayerActive) => prevPlayerActive === 'X' ? 'O' : 'X');
-
     setGameTurns((prevGameTurns) => {
-      let activePlayer = 'X';
+      const activePlayer = setActivePlayer(prevGameTurns);
 
-      if(gameTurns.length > 0 && prevGameTurns[0].player === 'X') {
-        activePlayer = 'O';
-      }
       const updatedGameTurns = [{ square: { row: rowIndex, col: colIndex }, player: activePlayer }, ...prevGameTurns]
-      
-      return updatedGameTurns; 
+
+      return updatedGameTurns;
     })
   }
 
@@ -26,12 +32,12 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" isActive={playerActive === 'X'} symbol="X" />
-          <Player initialName="Player 2" isActive={playerActive === 'O'} symbol="O" />
+          <Player initialName="Player 1" isActive={currentPlayer === 'X'} symbol="X" />
+          <Player initialName="Player 2" isActive={currentPlayer === 'O'} symbol="O" />
         </ol>
         <GameBoard onChange={handlerOnChange} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
